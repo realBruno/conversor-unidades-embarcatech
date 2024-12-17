2fe70463d61263ceb608aca332ca1c4b190b353a
realBruno <soubruno@protonmail.com>
2024-12-17 07:53:30 -0300

    Add comprimento.c and refactor temperature.c

---
+++ comprimento.c
@@ -0,0 +1,148 @@
+// Conversor de comprimento
+// 17/12/2024
+
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// PROTÓTIPO DAS FUNÇÕES
+double metros(double x, int y);
+double centimetros(double x, int y);
+double milimetros(double x, int y);
+
+int main(void)
+{
+    printf("PROGRAMA PARA CONVERTER PARA METRO, CENTÍMETRO OU MILÍMETRO\n");
+    sleep(1); // delay de um segundo
+    printf("Exemplos de entradas válidas: 30M | 70.8cm | 10.2MM\n");
+    sleep(1);
+
+    double comp_original;
+    char unidade_original;
+    printf("Insira o comprimento com a formatação especificada: ");
+    scanf("%lf%1c", &comp_original, &unidade_original);
+    unidade_original = tolower(unidade_original);
+
+    char c;
+    int mm = 0;
+    while ((c = getchar()) != '\n' && c != EOF)
+    {   
+        // se houver m no buffer, usuário inputou mm
+        if (c == 'm')
+        {
+            mm = 1;
+        } 
+    }
+
+    double comp_convertido;
+    int unidade_convertida;
+    
+    if (unidade_original == 'm' && mm != 1) // metro
+    {
+        printf("Deseja converter para centímetro (1) ou milímetro (2)? ");
+        scanf("%d", &unidade_convertida);
+        comp_convertido = metros(comp_original, unidade_convertida);
+    }
+    else if (unidade_original == 'c') // centímetro
+    {
+        printf("Deseja converter para metro (1) ou milímetro (2)? ");
+        scanf("%d", &unidade_convertida);
+        comp_convertido = centimetros(comp_original, unidade_convertida);
+    }
+    else if (unidade_original == 'm' && mm == 1) // milímetro
+    {
+        printf("Deseja converter para metro (1) ou centímetro (2)? ");
+        scanf("%d", &unidade_convertida);
+        comp_convertido = milimetros(comp_original, unidade_convertida);
+    }
+    else
+    {
+        printf("Entrada inválida.\n");
+        printf("Encerrando...\n");
+        sleep(1);
+        exit(EXIT_FAILURE); // encerra o programa
+    }
+
+    printf("%.2lf%c equivale a %.2lf", 
+            comp_original, 
+            unidade_original, 
+            comp_convertido
+        );
+
+    if (unidade_original == 'm' && mm != 1)
+    {
+        if (unidade_convertida == 1)
+            printf("%s\n", "cm");
+        else
+            printf("%s\n", "mm");
+    }
+    else if (unidade_original == 'c')
+    {
+        if (unidade_convertida == 1)
+            printf("%c\n", 'm');
+        else
+            printf("%s\n", "mm");
+    }
+    else if(unidade_original == 'm')
+    {
+        if (unidade_convertida == 1)
+            printf("%c\n", 'm');
+        else
+            printf("%s\n", "cm");
+    }
+    
+    return 0;
+} /* FIM DA FUNÇÃO MAIN */
+
+
+
+
+// DEFINIÇÃO DAS FUNÇÕES
+double metros(double x, int y)
+{   
+    double convertido;
+
+    if (y == 1) // metro para centímetro
+    {   
+        convertido = x * 100;
+    }
+    else // metro para milimetro
+    {
+        convertido = x * 1000;
+    }
+
+    return convertido;
+}
+
+double centimetros(double x, int y)
+{   
+    double convertido;
+
+    if (y == 1) // centimetro para metro
+    {
+        convertido = x/100.0f;
+    }
+    else // centimetro para milimetro
+    {
+        convertido = x * 10;
+    }
+
+    return convertido;
+}
+
+double milimetros(double x, int y)
+{   
+    double convertido;
+    
+    if (y == 1) // milimetro para metro
+    {
+        convertido = x/1000.0f;
+    }
+    else // milimetro para centimetro
+    {
+        convertido = x/10.0f;
+    }
+
+    return convertido;
+}
---
+++ temperatura.c
@@ -0,0 +1,116 @@
+// Conversor de temperatura
+// 16/12/2024
+
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// PROTÓTIPO DAS FUNÇÕES
+double celsius(double x, char y);
+double fahrenheit(double x, char y);
+double kelvin(double x, char y);
+
+int main(void)
+{   
+    printf("PROGRAMA PARA CONVERTER PARA CELSIUS, FAHRENHEIT OU KELVIN\n");
+    sleep(1); // delay de um segundo
+    printf("Exemplos de entradas válidas: 30C | 70.8F | 10.2K\n");
+    sleep(1);
+
+    double temp_original;
+    char unidade_original;
+    printf("Insira a temperatura com a formatação especificada: ");
+    scanf("%lf%c", &temp_original, &unidade_original);
+    unidade_original = toupper(unidade_original);
+    
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) // limpa o buffer
+        ;
+
+    double temp_convertida;
+    char unidade_convertida;
+
+    switch(unidade_original)
+    {
+        case 'C':
+            printf("Deseja converter para Fahrenheit ou Kelvin? (F/K): ");
+            scanf("%c", &unidade_convertida);
+            unidade_convertida = toupper(unidade_convertida);
+            temp_convertida = celsius(temp_original, unidade_convertida);
+            break;
+        case 'F':
+            printf("Deseja converter para Celsius ou Kelvin? (C/K): ");
+            scanf("%c", &unidade_convertida);
+            unidade_convertida = toupper(unidade_convertida);
+            temp_convertida = fahrenheit(temp_original, unidade_convertida);
+            break;
+        case 'K':
+            printf("Deseja converter para Celsius ou Fahrenheit? (C/F): ");
+            scanf("%c", &unidade_convertida);
+            unidade_convertida = toupper(unidade_convertida);
+            temp_convertida = kelvin(temp_original, unidade_convertida);
+            break;
+        default:
+            printf("Entrada inválida.\n");
+            printf("Encerrando...\n");
+            sleep(1);
+            exit(EXIT_FAILURE); // encerra o programa
+    }
+
+    printf("%.2lf%c equivale a %.2lf%c\n", temp_original, unidade_original, 
+        temp_convertida, unidade_convertida);
+
+    return 0;
+}
+
+// DEFINIÇÃO DAS FUNÇÕES
+double celsius(double x, char y)
+{   
+    double temp_convertida;
+
+    if (y == 'F') // celsius para fahrenheit
+    {   
+     temp_convertida = (x * 1.8f) + 32;
+    }
+    else if (y == 'K') // celsius para kelvin
+    {   
+        temp_convertida = x + 273.15;
+    }
+
+    return temp_convertida;
+}
+
+double fahrenheit(double x, char y)
+{   
+    double temp_convertida;
+
+    if (y == 'C') // fahrenheit para celsius
+    {   
+        temp_convertida = (x - 32.0)/1.8f;
+    }
+    else if (y == 'K') // fahrenheit para kelvin
+    {   
+        temp_convertida = (x - 32)/1.8f;
+        temp_convertida += 273.15f;
+    }
+
+    return temp_convertida;
+}
+
+double kelvin(double x, char y)
+{   
+    double temp_convertida;
+
+    if (y == 'F') // kelvin para fahrenheit
+    {
+        temp_convertida = ((x - 273.15f) * 1.8f) + 32;
+    }
+    else if (y == 'C') // kelvin para celsius
+    {
+        temp_convertida = x - 273.15f;
+        temp_convertida = temp_convertida * 1.8f + 32;
+    }
+
+    return temp_convertida;
+}
